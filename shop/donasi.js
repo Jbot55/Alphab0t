8fc7c02d98af16a483756f0e26859d28afd34e6a
MENGHAYYU <81160542+Jbot55@users.noreply.github.com>
2022-04-01 19:26:54 +0800

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -4,24 +4,24 @@ return `DONASI BISA VIA BANK / E-MONEY
 *🏧 BANK*
 
 1. BRI 
-	ㅁ 8881 0887 4350 47326
+	ㅁ -
 2. BCA 
-	ㅁ 3901 0887 4350 47326
+	ㅁ 3-
 3. BNI 
-	ㅁ 881 0887 4350 47326
+	ㅁ -
 4. PERMATA
-	ㅁ 852 8887 4350 47326
+	ㅁ -
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ 0895 6050 30379
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 0895 6050 30379
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ  0895 6050 30379
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama JU****i
 `
 	}
 
